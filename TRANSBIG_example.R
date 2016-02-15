e86433609ec637127885bb5f997a0280f5b04ba6
whcsu <whcsu@users.noreply.github.com>
2016-02-15 09:37:52 +0800

    Update TRANSBIG_example.R

--- TRANSBIG_example.R
+++ TRANSBIG_example.R
@@ -8,19 +8,14 @@ library(MASS)
 library(gbm)
 library(Matrix)
 library(survival)
-#library(Hmisc)
+
 library(survcomp)
 library(survivalROC)
-#library(ff)
 
 #for High-dimensional 
 library(affy)
-#for dnet TCGA_mutations data
-#library(foreach)
-#library(doSNOW)
 
-#cl <- makeCluster(2, type="SOCK")
-#registerDoSNOW(cl)
+source("funs-sesrp.R")
 
 
 
@@ -36,7 +31,7 @@ transpdata=transpdata[ ,colnames(transpdata)[c(6,5,1:4,7:9)]]
 colnames(transpdata)[1] <- "time"
 colnames(transpdata)[2] <- "status"
 
-# log 2 transform
+
 tm=ExpressionSet(assayData=(exprs(transbig)),phenoData=transpdata)
 #delete all missing values
 
@@ -52,38 +47,15 @@ rm(transbig)
 tm=tm[,pData(tm)$time>0]
 
 
-
-#save(tm,file="mainz.RData")
-#sample covariates 3096 samples * 7 covariates 
-#pd=pData(TCGA_mutations)
-#pd=pData(tm)
-#gene expression data
-#time=pd[,1]
-#status=pd[,2]
-#pd=pd[,-c(1,2)]
-
-#save(tm,file="tm.RData")
-
-
-
-#write.csv(tm,"tm.csv")
-
-
 n=dim(tm)[2]
 
 
 Rn=10
-#testresult <-data.frame(row.names=1:Rn)
 
-#AUC
 
 #AUC
 ci_srp<-c(rep(0,Rn))
 
-#ci_rsf<-c(rep(0,Rn))
-#ci_cox<-c(rep(0,Rn))
-#ci_gbm<-c(rep(0,Rn))
-source("funs-srp.R")
 
 for (i in seq(1, Rn, by=2))
 {
@@ -135,11 +107,6 @@ for (i in seq(1, Rn, by=2))
   
   
 }
-
-
-
-#testresult=cbind(testresult,data.frame(unlist(ci_rotsf),unlist(ci_rsf),unlist(ci_cox),unlist(ci_gbm)))
-
 csvfn=paste0("srp-",datasetname,"-n5.csv")
 write.csv(ci_srp,csvfn)
 
